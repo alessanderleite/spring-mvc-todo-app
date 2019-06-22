59cdb9a6cd5eb9892761b2eb0ac892fa3352d6f8
alessanderleite <alessander.logistica@gmail.com>
2019-06-21 22:41:16 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/app/SpringMvcTodoAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcTodoAppApplication.class, args);
+	}
+
+}
