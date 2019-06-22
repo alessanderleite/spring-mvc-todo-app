28c329779a420e52766ad5c6685137ee633cac8b
alessanderleite <alessander.logistica@gmail.com>
2019-06-22 15:10:29 -0300

    create the model class named Todo

---
+++ src/main/java/br/com/alessanderleite/app/model/Todo.java
@@ -0,0 +1,69 @@
+package br.com.alessanderleite.app.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String userName;
+	
+	@Size(min = 10, message = "Enter at least 10 Characters...")
+	private String descripiton;
+	
+	private Date targetDate;
+
+	public Todo() {
+		super();
+	}
+
+	public Todo(String user, String desc, Date targetDate, boolean isDone) {
+		super();
+		this.userName = user;
+		this.descripiton = desc;
+		this.targetDate = targetDate;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getDescripiton() {
+		return descripiton;
+	}
+
+	public void setDescripiton(String descripiton) {
+		this.descripiton = descripiton;
+	}
+
+	public Date getTargetDate() {
+		return targetDate;
+	}
+
+	public void setTargetDate(Date targetDate) {
+		this.targetDate = targetDate;
+	}	
+}
