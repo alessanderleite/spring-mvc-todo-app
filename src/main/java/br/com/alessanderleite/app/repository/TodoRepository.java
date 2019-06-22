f298f8fbedfc09d40d419d215020018ac7f9aabf
alessanderleite <alessander.logistica@gmail.com>
2019-06-22 20:38:51 -0300

    create the repository

---
+++ src/main/java/br/com/alessanderleite/app/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package br.com.alessanderleite.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alessanderleite.app.model.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+	List<Todo> findUserName(String user);
+}
