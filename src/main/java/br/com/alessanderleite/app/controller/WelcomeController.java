21a737ab038286df53307214661e565d42e63c43
alessanderleite <alessander.logistica@gmail.com>
2019-06-23 23:05:18 -0300

    create the empty class named WelcomeController

---
+++ src/main/java/br/com/alessanderleite/app/controller/WelcomeController.java
@@ -0,0 +1,9 @@
+package br.com.alessanderleite.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+}
